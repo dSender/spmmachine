c0e3f5e9d087e4d141456267d15798e039cf7fe1
dSender <44706091+dSender@users.noreply.github.com>
2019-05-04 09:27:19 +0400

    Add files via upload

--- startBOT.py
+++ startBOT.py
@@ -7,11 +7,12 @@ import re
 
 proxy = {'https':'https://157.230.240.140:8080'}
 
-def run_appl(phone):
-    yan(phone)
-    ok(phone)
-    inst(phone)
-    auto_ru(phone)
+
+def run_appl(phone,i):
+    yan(phone,i)
+    ok(phone,i)
+    inst(phone,i)
+    auto_ru(phone,i)
 
 def code_rev(code):
     last_code = code[0]
@@ -20,21 +21,22 @@ def code_rev(code):
         if str(text) == last_code:
             pass
         else:
+
             with open('code.base', 'w+') as file:
                 file.write(last_code)
             for i in range(int(last_code)+1):
-                run_appl(code[1])
-                sleep(5)
-
-
+                run_appl(code[1],i)
 
 def main():
-    request = str(requests.get('https://api.telegram.org/bot <> /getUpdates', proxies = proxy).text)
-    compiler_ = re.compile(r'"text":".+",')
-    last_update_ = re.findall(compiler_, request)[-1]
-    code_compil_ = re.compile(r'\d+')
-    code = re.findall(code_compil_, last_update_)
-    return code
+    while 1:
+        request = str(requests.get('https://api.telegram.org/bot  <>  /getUpdates', proxies = proxy).text) # ******
+        compiler_ = re.compile(r'"text":".+",')
+        last_update_ = re.findall(compiler_, request)
+        if len(last_update_) > 0:
+            last_update_ = last_update_[-1]
+            code_compil_ = re.compile(r'\d+')
+            code = re.findall(code_compil_, last_update_)
+            return code
 
 while 1:
     code_rev(main())
